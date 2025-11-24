8cd40db8d9fbde899cdc4eace5b993fafd82defd
AdminMel <74563324+AdminMel@users.noreply.github.com>
2025-11-24 07:34:27 -0600

    Update EntrenadorService.java

--- ligas/src/main/java/com/upiiz/ligas/services/EntrenadorService.java
+++ ligas/src/main/java/com/upiiz/ligas/services/EntrenadorService.java
@@ -17,7 +17,22 @@ public class EntrenadorService {
         return entrenadorRepository.findAll();
     }
 
-    public Entrenador save(Entrenador entrenador) {
+    public Entrenador findById(Long id) {
+        return entrenadorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Entrenador no encontrado con id " + id));
+    }
+
+    public Entrenador create(Entrenador entrenador) {
         return entrenadorRepository.save(entrenador);
+    }
+
+    public Entrenador update(Long id, Entrenador entrenador) {
+        Entrenador existente = findById(id);
+        entrenador.setId(existente.getId());
+        return entrenadorRepository.save(entrenador);
+    }
+
+    public void delete(Long id) {
+        entrenadorRepository.deleteById(id);
     }
 }
